1501e37d382024914994dee3a3000d4f58039f89
Holodome <ilal.vinogradov@gmail.com>
2024-04-09 21:39:26 +0300

    start writing ui

---
+++ web/src/routes/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<nav>
+    <a href="/">Home</a>
+
+    {#if $page.data.user}
+        <a href="/account">Account</a>
+    {:else}
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    {/if}
+</nav>
---
+++ web/src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { enhance } from '$app/forms';
+    /** @type {import('./$types').ActionData} */
+	export let form;
+</script>
+
+<svelte:head>
+	<title>Sign in</title>
+</svelte:head>
+
+<div>
+    <form use:enhance method="POST">
+        <fieldset>
+            <input name="username" type="text" required placeholder="Username"/>
+        </fieldset>
+        <fieldset>
+            <input name="password" type="password" required placeholder="Password"/>
+        </fieldset>
+        <button type="submit">Login</button>
+    </form>
+</div>

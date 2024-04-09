af0c07eeca54c7a4c6788359f4d0d85b23bff006
Holodome <ilal.vinogradov@gmail.com>
2024-04-09 22:52:21 +0300

    rewrite ioLoggedTest definition to be more concise

--- modules/tests/src/main/scala/com/holodome/IOLoggedTest.scala
+++ modules/tests/src/main/scala/com/holodome/IOLoggedTest.scala
@@ -1,9 +1,11 @@
 package com.holodome
 
 import cats.effect.IO
-import weaver.{Expectations, Log, SimpleIOSuite, Test, TestName}
+import org.typelevel.log4cats.Logger
+import weaver.{Expectations, Log, SimpleIOSuite, TestName}
 
 abstract class IOLoggedTest extends SimpleIOSuite {
-  def ioLoggedTest(name: TestName)(run: WeaverLogAdapter[IO] => IO[Expectations]): Unit =
-    registerTest(name)(_ => Test(name.name, (log: Log[IO]) => run(new WeaverLogAdapter[IO](log))))
+  def ioLoggedTest(name: TestName)(run: Logger[IO] => IO[Expectations]): Unit =
+    loggedTest(name)(run.compose[Log[IO]](new WeaverLogAdapter[IO](_)))
+
 }

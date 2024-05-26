9672ff990b911e9137897ef8633f6ce6b7efd6a7
Holodome <ilal.vinogradov@gmail.com>
2024-05-26 12:18:39 +0300

    finish tags and gallery on ad page

--- web/src/routes/ads/[ad]/+page.svelte
+++ web/src/routes/ads/[ad]/+page.svelte
@@ -29,48 +29,34 @@
   <div class="container mb-8">
     <h2 class="text-lg leading-none mt-8 mb-4">Gallery</h2>
     <div class="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box mb-4">
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-          class="rounded-box"
-        />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-          class="rounded-box"
-        />
-      </div>
+      {#each data.images as img}
+        <div class="carousel-item relative">
+          <img src={img.url} alt="ad" class="rounded-box" width="300" height="300" />
+          {#if isAuthor()}
+            <form use:enhance method="POST" action="?/delete_image">
+              <input type="hidden" name="image" value={img.id} />
+              <button
+                class="btn btn-circle btn-outline absolute -translate-x-full"
+                type="submit"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  /></svg
+                >
+              </button>
+            </form>
+          {/if}
+        </div>
+      {/each}
     </div>
     {#if isAuthor()}
       <form use:enhance method="POST" action="?/add_image" enctype="multipart/form-data">
@@ -88,17 +74,19 @@
 
   <div class="container mb-8">
     <h2 class="text-lg leading-none mt-8 mb-4">Tags</h2>
-    {#each data.adInfo.tags as tag}
-      <div>
-        {tag}
-        {#if isAuthor()}
-          <form use:enhance method="POST" action="?/delete_tag" style="display: inline-block;">
-            <input type="hidden" name="tag" value={tag} />
-            <button type="submit" class="btn btn-outline btn-error">Delete</button>
-          </form>
-        {/if}
-      </div>
-    {/each}
+    <ul class="list-disc list-inside mb-4">
+      {#each data.adInfo.tags as tag}
+        <li>
+          {tag}
+          {#if isAuthor()}
+            <form use:enhance method="POST" action="?/delete_tag" class="inline-block ml-4">
+              <input type="hidden" name="tag" value={tag} />
+              <button type="submit" class="btn btn-outline btn-error btn-sm">Delete</button>
+            </form>
+          {/if}
+        </li>
+      {/each}
+    </ul>
     {#if isAuthor()}
       <form use:enhance method="POST" action="?/add_tag">
         <input type="text" name="tag" class="input input-bordered w-full max-w-xs" />
@@ -106,18 +94,6 @@
       </form>
     {/if}
   </div>
-
-  {#each data.images as img}
-    <div style="position: relative;">
-      <img src={img.url} alt="ad" width="300" height="300" />
-      {#if isAuthor()}
-        <form use:enhance method="POST" action="?/delete_image">
-          <input type="hidden" name="image" value={img.id} />
-          <button type="submit" style="position: absolute; bottom: 10px; left: 10px">Delete</button>
-        </form>
-      {/if}
-    </div>
-  {/each}
 
   {#if $page.data.user && !isAuthor()}
     <form use:enhance method="POST" action="?/create_chat">

fa6f8d81d675a8a98ea7de3bf7ffaef5eb370a62
Holodome <ilal.vinogradov@gmail.com>
2024-10-04 10:56:01 +0300

    split minio client into policy and mechanism

---
+++ modules/core/src/main/scala/maweituo/infrastructure/minio/MinioClient.scala
@@ -0,0 +1,71 @@
+package maweituo.infrastructure.minio
+
+import java.io.InputStream
+
+import scala.util.control.NonFatal
+
+import cats.data.OptionT
+import cats.effect.Async
+import cats.syntax.all.*
+
+import maweituo.ext.catsExt.liftCompletableFuture
+import maweituo.infrastructure.{OBSId, OBSUrl}
+
+import io.minio.*
+import io.minio.errors.ErrorResponseException
+
+class MinioClient[F[_]: Async](connection: MinioConnection):
+
+  def baseUrl: String          = connection.baseUrl
+  def client: MinioAsyncClient = connection.client
+
+  def putStream(bucket: String, id: OBSId, blob: fs2.Stream[F, Byte], dataSize: Long): F[Unit] =
+    fs2.io.toInputStreamResource(blob).use { is =>
+      liftCompletableFuture {
+        client
+          .putObject(
+            PutObjectArgs
+              .builder()
+              .bucket(bucket)
+              .`object`(id.value)
+              .contentType("binary/octet-stream")
+              .stream(is, dataSize, -1)
+              .build()
+          )
+      }.void
+    }
+
+  def get(bucket: String, id: OBSId): OptionT[F, fs2.Stream[F, Byte]] =
+    OptionT(
+      liftCompletableFuture(
+        client.getObject(GetObjectArgs.builder().bucket(bucket).`object`(id.value).build())
+      ).map(_.some).recoverWith {
+        case NonFatal(e: ErrorResponseException) if e.errorResponse().code() == "NoSuchKey" =>
+          none[GetObjectResponse].pure[F]
+      }
+    ).map { (resp: InputStream) =>
+      fs2.io.readInputStream(resp.pure[F], 4096)
+    }
+
+  def delete(bucket: String, id: OBSId): F[Unit] =
+    liftCompletableFuture(
+      client.removeObject(RemoveObjectArgs.builder().bucket(bucket).`object`(id.value).build())
+    ).void
+
+  def bucketExists(bucket: String): F[Boolean] =
+    liftCompletableFuture(
+      client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build())
+    ).map(scala.Boolean.unbox)
+
+  def createBucket(bucket: String): F[Unit] =
+    liftCompletableFuture(
+      client.makeBucket(
+        MakeBucketArgs
+          .builder()
+          .bucket(bucket)
+          .build()
+      )
+    ).void
+
+  def makeUrl(bucket: String, id: OBSId): OBSUrl =
+    OBSUrl(s"$baseUrl/$bucket/${id.value}")
--- modules/core/src/main/scala/maweituo/infrastructure/minio/MinioObjectStorage.scala
+++ modules/core/src/main/scala/maweituo/infrastructure/minio/MinioObjectStorage.scala
@@ -1,89 +1,60 @@
 package maweituo.infrastructure.minio
-
-import java.io.InputStream
-
-import scala.util.control.NonFatal
-
 import cats.data.OptionT
 import cats.effect.Async
 import cats.syntax.all.*
 
-import maweituo.ext.catsExt.liftCompletableFuture
 import maweituo.infrastructure.{OBSId, OBSUrl, ObjectStorage}
 
 import _root_.org.typelevel.log4cats.Logger
-import io.minio.*
-import io.minio.errors.ErrorResponseException
 
-final case class MinioConnection(baseUrl: String, client: MinioAsyncClient)
+final case class MinioConnection(baseUrl: String, client: io.minio.MinioAsyncClient)
 
 object MinioObjectStorage:
   def make[F[_]: Async: Logger](
       conn: MinioConnection,
       bucket: String
   ): F[ObjectStorage[F]] =
-    ensureBucketIsCreated(conn.client, bucket).as(new MinioObjectStorage(conn.baseUrl, conn.client, bucket))
+    val client = new MinioClient[F](conn)
+    ensureBucketIsCreated(client, bucket).as(new MinioObjectStorage(client, bucket))
 
   private def ensureBucketIsCreated[F[_]: Async: Logger](
-      minio: MinioAsyncClient,
+      minio: MinioClient[F],
       bucket: String
-  ): F[Unit] = liftCompletableFuture(
-    minio.bucketExists(BucketExistsArgs.builder().bucket(bucket).build())
-  ).map(scala.Boolean.unbox)
-    .flatMap {
+  ): F[Unit] =
+    minio.bucketExists(bucket).flatMap {
       case true => Logger[F].info(f"Bucket $bucket already exists")
       case false =>
-        Logger[F].info(f"Bucket $bucket does not exist, creating") *> liftCompletableFuture(
-          minio.makeBucket(
-            MakeBucketArgs
-              .builder()
-              .bucket(bucket)
-              .build()
-          )
-        ).void *> Logger[F].info(f"Bucket $bucket created")
+        for
+          _ <- Logger[F].info(f"Bucket $bucket does not exist, creating")
+          _ <- minio.createBucket(bucket)
+          _ <- Logger[F].info(f"Bucket $bucket created")
+        yield ()
     }.recoverWith {
       case e =>
         Logger[F].warn(e)("Error when creating bucket")
     }
 
 private final class MinioObjectStorage[F[_]: Async: Logger](
-    baseUrl: String,
-    client: MinioAsyncClient,
+    client: MinioClient[F],
     bucket: String
 ) extends ObjectStorage[F]:
 
   override def putStream(id: OBSId, blob: fs2.Stream[F, Byte], dataSize: Long): F[Unit] =
-    Logger[F].info(f"Putting blob $id of size $dataSize") *> fs2.io.toInputStreamResource(blob).use { is =>
-      liftCompletableFuture {
-        client
-          .putObject(
-            PutObjectArgs
-              .builder()
-              .bucket(bucket)
-              .`object`(id.value)
-              .contentType("binary/octet-stream")
-              .stream(is, dataSize, -1)
-              .build()
-          )
-      }.void
-    } <* Logger[F].info(f"Finished putting blob $id")
+    for
+      _ <- Logger[F].info(f"Putting blob $id of size $dataSize")
+      x <- client.putStream(bucket, id, blob, dataSize)
+      _ <- Logger[F].info(f"Finished putting blob $id")
+    yield x
 
   override def get(id: OBSId): OptionT[F, fs2.Stream[F, Byte]] =
-    OptionT(
-      liftCompletableFuture(
-        client.getObject(GetObjectArgs.builder().bucket(bucket).`object`(id.value).build())
-      ).map(_.some).recoverWith {
-        case NonFatal(e: ErrorResponseException) if e.errorResponse().code() == "NoSuchKey" =>
-          none[GetObjectResponse].pure[F]
-      }
-    ).map { (resp: InputStream) =>
-      fs2.io.readInputStream(resp.pure[F], 4096)
-    }
+    client.get(bucket, id)
 
   override def delete(id: OBSId): F[Unit] =
-    Logger[F].info(f"Deleting blob $id") *> liftCompletableFuture(
-      client.removeObject(RemoveObjectArgs.builder().bucket(bucket).`object`(id.value).build())
-    ).void <* Logger[F].info(f"Deleted blob $id")
+    for
+      _ <- Logger[F].info(f"Deleting blob $id")
+      _ <- client.delete(bucket, id)
+      _ <- Logger[F].info(f"Deleted blob $id")
+    yield ()
 
   override def makeUrl(id: OBSId): OBSUrl =
-    OBSUrl(s"$baseUrl/$bucket/${id.value}")
+    client.makeUrl(bucket, id)
